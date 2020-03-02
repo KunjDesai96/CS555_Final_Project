1dd0442d31e76675e4a1e657df6c1da3d5619fad
Nihir <nihir108@gmail.com>
2020-03-02 13:56:40 -0500

    Update GenerateOutput.java

--- FinalProject/src/edu/stevens/cs555/GenerateOutput.java
+++ FinalProject/src/edu/stevens/cs555/GenerateOutput.java
@@ -374,6 +374,80 @@ public class GenerateOutput {
 				return false;
 
 		}
+		
+		/**
+		 * Author: Nihir Patel
+			 * ID: US18
+			 * Name: Siblings should not marry
+			 * Description: siblings should not marry
+			 * Date created: Feb 27, 20201:23:06 AM
+			 * @throws ParseException 
+		 */
+		public static boolean us18_siblings_should_not_marry () throws ParseException
+		{
+			boolean flag = true;
+			for (Iterator<Entry<String, FamilyEntry>> iteratorFam = hfam.entrySet().iterator(); iteratorFam
+					.hasNext();) {
+				Entry<String, FamilyEntry> famMapElement = iteratorFam.next();
+				String keyFam = famMapElement.getKey().trim();
+				FamilyEntry famValue = famMapElement.getValue();
+				String H_id=famValue.getH_id().trim();
+				String W_id=famValue.getW_id().trim();
+				if(hind.get(H_id).getChild().size()!=0 && hind.get(W_id).getChild().size()!=0) {
+					if(hind.get(H_id).getChild().equals(hind.get(W_id).getChild())) {
+						flag=false;
+						String failStr = "us18_siblings_should_not_marry: "+hind.get(H_id).getName().trim()+" should not marry his sibling "+hind.get(W_id).getName().trim();
+						failures.add(failStr);
+						failuresFlag = true;
+					}
+				}
+				
+//				if(indValue.getGender().equals("M") && !indValue.spous.isEmpty()) {
+//					String famID;
+//					for (Iterator<String> it = indValue.spous.iterator(); it.hasNext(); ) {
+//						famID=it.next();
+//					}
+//					
+				}
+			
+			
+			return flag;
+		}
+		/**
+		 * Author: Nihir Patel
+			 * ID: US16
+			 * Name: Male Last name
+			 * Description: All male members of a family should have the same last name
+			 * Date created: Feb 27, 20201:23:06 AM
+			 * @throws ParseException 
+		 */
+		public static boolean us16_Male_last_name () throws ParseException
+		{
+			boolean flag = true;
+			for (Iterator<Entry<String, FamilyEntry>> iteratorFam = hfam.entrySet().iterator(); iteratorFam
+					.hasNext();) {
+				Entry<String, FamilyEntry> famMapElement = iteratorFam.next();
+				String keyFam = famMapElement.getKey().trim();
+				FamilyEntry famValue = famMapElement.getValue();
+				String famlastname=hind.get(famValue.getH_id().trim()).getName().split("/")[1];
+				for (Iterator<String> it = famValue.getChild().iterator(); it.hasNext(); ) {
+					IndividualEntry indValue=  hind.get(it.next().trim());
+					String indlastname=indValue.getName().split("/")[1];
+					
+					if(indValue.getGender().trim().equals("M") && !indlastname.equals(famlastname)) {
+						flag=false;
+						String failStr = "us16_Male_last_name: "+indValue.getName().trim()+"  and "+hind.get(famValue.getH_id().trim()).getName()+" does not have same last name!";
+						failures.add(failStr);
+						failuresFlag = true;
+					}
+				}
+				
+			}
+			return flag;
+			
+		}
+		
+		
 //====================================================== End of user stories ======================================================
 
 	/**
